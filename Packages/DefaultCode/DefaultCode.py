37f1ae3be0d616d32c9d902ad3ab2d18c325cce1
vimi <vimiming@gmail.com>
2018-11-05 21:04:49 +0800

    修改go_code，变成一个返回json的接口

--- Packages/DefaultCode/DefaultCode.py
+++ Packages/DefaultCode/DefaultCode.py
@@ -222,17 +222,25 @@ int main() {
 		code = """package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type vimi struct {
+	Name string `json:"json_name"`
+	ID   string `json:"json_id"`
+}
+
 func root(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() //解析参数，默认是不会解析的
+	r.ParseForm()                                      //解析参数，默认是不会解析的
 	w.Header().Add("Access-Control-Allow-Origin", "*") //设置跨域
 	fmt.Println("get message from " + r.RemoteAddr)
 	fmt.Println(r.Form)
-	fmt.Fprintf(w, "I get your message!") //这个写入到w的是输出到客户端的
+	v := &vimi{`vimi`, `ming`}
+	jsonValueOfV, _ := json.Marshal(v)
+	fmt.Fprintf(w, string(jsonValueOfV)) //这个写入到w的是输出到客户端的
 }
 
 func main() {
