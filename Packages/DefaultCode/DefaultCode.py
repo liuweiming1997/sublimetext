3f6603405317814e36d28a40d77c11e9410d3cde
vimiPonyai <weimingliu@pony.ai>
2018-11-12 09:52:03 +0800

    增加protoc的code 删除PackageResourceViewer

--- Packages/DefaultCode/DefaultCode.py
+++ Packages/DefaultCode/DefaultCode.py
@@ -34,10 +34,44 @@ class DefaultCodeCommand(sublime_plugin.TextCommand):
 			self.yaml_code(edit)
 		elif suffix == "h":
 			self.header_file_code(edit)
+		elif suffix == "proto":
+			self.proto_code(edit)
 		else: # 这个是makefile
 			self.error_code(edit)
 
+	def proto_code(self, edit):
+		code = """// See README.txt for information and build instructions.
 
+package tutorial;
+
+option java_package = "com.example.tutorial";
+option java_outer_classname = "AddressBookProtos";
+
+message Person {
+  required string name = 1;
+  required int32 id = 2;        // Unique ID number for this person.
+  optional string email = 3;
+
+  enum PhoneType {
+    MOBILE = 0;
+    HOME = 1;
+    WORK = 2;
+  }
+
+  message PhoneNumber {
+    required string number = 1;
+    optional PhoneType type = 2 [default = HOME];
+  }
+
+  repeated PhoneNumber phone = 4;
+}
+
+// Our address book file is just one of these.
+message AddressBook {
+  repeated Person person = 1;
+}
+"""
+		self.view.insert(edit, 0, code)
 	def header_file_code(self, edit):
 			code = """// We use this preprocessor directive to cause the current source file to be included only once
 // in a single compilation.
