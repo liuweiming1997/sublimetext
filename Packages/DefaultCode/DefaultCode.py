835a5817ef5a39636a635db35aae2ab3f137d5f2
vimiPonyai <weimingliu@pony.ai>
2018-11-19 22:00:48 +0800

    promise

--- Packages/DefaultCode/DefaultCode.py
+++ Packages/DefaultCode/DefaultCode.py
@@ -333,35 +333,38 @@ func main() {
   def js_code(self, edit):
     code = """'use strict'
 var url = 'http://95.163.202.160:8080';
-
-function success(text) {
-    var textarea = document.getElementById('id_test');
-    textarea.innerHTML = text;
-}
-
-function fail(code) {
-    var textarea = document.getElementById('id_test');
-    textarea.innerHTML = 'Error code: ' + code;
-}
-
-var request = new XMLHttpRequest(); // 新建Microsoft.XMLHTTP对象
-request.onreadystatechange = function() { // 状态发生变化时，函数被回调
-    if (request.readyState === 4) { // 成功完成
-        // 判断响应结果:
-        if (request.status === 200) {
-            // 成功，通过responseText拿到响应的文本:
-            return success(request.responseText);
+const getJSON = function(url) {
+  const promise = new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest(); // 新建Microsoft.XMLHTTP对象
+      request.onreadystatechange = function() { // 状态发生变化时，函数被回调
+        if (request.readyState === 4) { // 成功完成
+          // 判断响应结果:
+          if (request.status === 200) {
+              // 成功，通过responseText拿到响应的文本:
+              resolve(request.responseText);
+          } else {
+              // 失败，根据响应码判断失败原因:
+              reject(request.status);
+          }
         } else {
-            // 失败，根据响应码判断失败原因:
-            return fail(request.status);
+            // HTTP请求还在继续...
         }
-    } else {
-        // HTTP请求还在继续...
-    }
-}
-// 发送请求:
-request.open('GET', url);
-request.send();"""
+      }
+      // 发送请求:
+      request.open('GET', url);
+      request.send();
+    });
+
+  return promise;
+};
+
+getJSON(url).then(function(json) {
+  alert('Contents: ' + json);
+}, function(error) {
+  alert('出错了', error);
+});
+
+alert("function");"""
     self.view.insert(edit, 0, code)
 
   def error_code(self, edit):
