a89ec205c666a54ac369e8b69f24ff91c6e248b1
vimiPonyai <weimingliu@pony.ai>
2018-12-17 09:54:09 +0800

    修改go的default_code && 15号字体在虚拟机才能不偏移??

--- Packages/DefaultCode/DefaultCode.py
+++ Packages/DefaultCode/DefaultCode.py
@@ -407,37 +407,101 @@ int main(int argc, char *argv[]) {
     self.view.insert(edit, 0, code)
 
   def go_code(self, edit):
-    code = """package main
+    code = '''package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+  "fmt"
+  "io"
+  "io/ioutil"
+  "log"
+  "net/http"
+  "os"
 )
 
-type vimi struct {
-	Name string `json:"json_name"`
-	ID   string `json:"json_id"`
+const (
+  rootPage = `
+  <!doctype html>
+  <html>
+  <body>
+  <form action='/saveImage' method='post' enctype='multipart/form-data'>
+     <input type='file' name='vimi_image'>
+     <input type='submit' value='Upload'>
+  </form>
+  `
+  savePath = `./img/`
+)
+
+func inWhereAndThenShowCookies(name string, r *http.Request) {
+  fmt.Println(name)
+  for k, v := range r.Cookies() {
+    fmt.Println(k, v)
+  }
+  fmt.Println("---->\\n")
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()                                      //解析参数，默认是不会解析的
-	w.Header().Add("Access-Control-Allow-Origin", "*") //设置跨域
-	fmt.Println("get message from " + r.RemoteAddr)
-	fmt.Println(r.Form)
-	v := &vimi{`vimi`, `ming`}
-	jsonValueOfV, _ := json.Marshal(v)
-	fmt.Fprintf(w, string(jsonValueOfV)) //这个写入到w的是输出到客户端的
+  inWhereAndThenShowCookies("in root", r)
+  w.Header().Add("Access-Control-Allow-Origin", "*") //设置跨域
+  r.ParseForm()                                      //解析参数，默认是不会解析的
+  fmt.Fprintf(w, rootPage)                           //这个写入到w的是输出到客户端的
+}
+
+func saveImage(w http.ResponseWriter, r *http.Request) {
+  inWhereAndThenShowCookies("in saveImage", r)
+  fmt.Println("in saveImage")
+  r.ParseForm()
+  uploadFile, handle, err := r.FormFile("vimi_image")
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  os.Mkdir(savePath, 0777)
+  saveFile, err := os.OpenFile(savePath+handle.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  io.Copy(saveFile, uploadFile)
+  defer uploadFile.Close()
+  defer saveFile.Close()
+
+  // must set it to response, and use in next request
+  // http.SetCookie(w, &http.Cookie{
+  //  Name:    "fileName",
+  //  Value:   handle.Filename,
+  //  Expires: time.Now().Add(24 * time.Second),
+  // })
+  http.Redirect(w, r, "/showImage?fileName="+handle.Filename, http.StatusFound)
+}
+
+func showImage(w http.ResponseWriter, r *http.Request) {
+  inWhereAndThenShowCookies("in showImage", r)
+  r.ParseForm()
+  fileName := r.URL.Query().Get("fileName")
+  file, err := os.Open(savePath + fileName)
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  defer file.Close()
+  buff, err := ioutil.ReadAll(file)
+  if err != nil {
+    log.Println(err)
+    return
+  }
+  w.Write(buff)
 }
 
 func main() {
-	http.HandleFunc("/", root)               //设置访问的路由
-	err := http.ListenAndServe(":8080", nil) //设置监听的端口
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}"""
+  http.HandleFunc("/", root)               //设置访问的路由
+  http.HandleFunc("/saveImage", saveImage) //设置访问的路由
+  http.HandleFunc("/showImage", showImage) //设置访问的路由
+  err := http.ListenAndServe(":8080", nil) //设置监听的端口
+  if err != nil {
+    log.Fatal("ListenAndServe: ", err)
+  }
+}
+'''
     self.view.insert(edit, 0, code)
 
   def js_code(self, edit):
