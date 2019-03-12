7e1854c6406497176ac3544abdb0696133561ced
vimi <vimiming@gmail.com>
2019-03-12 20:28:23 +0800

    cpp code

--- Packages/DefaultCode/DefaultCode.py
+++ Packages/DefaultCode/DefaultCode.py
@@ -400,11 +400,15 @@ using namespace std;
 #define inf (0x3f3f3f3f)
 typedef long long int LL;
 
+void work() {
+  
+}
+
 
 int main(int argc, char *argv[]) {
-    freopen("data.txt", "r", stdin);
-
-    return 0;
+  freopen("data.txt", "r", stdin);
+  work();
+  return 0;
 }
 """
     self.view.insert(edit, 0, code)
