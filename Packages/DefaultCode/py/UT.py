1ffd5f7337e4e246ee97535260c6df274d2deca1
vimiPonyai <weimingliu@pony.ai>
2019-12-01 21:42:46 +0800

    add py ut

---
+++ Packages/DefaultCode/py/UT.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class XXXTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_xxx(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
