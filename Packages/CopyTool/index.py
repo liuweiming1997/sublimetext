300536d1c6515d844bfb37c960d562a86108913c
weimingliu <vimiming@gmail.com>
2020-04-12 22:53:19 +0800

    clipboard helper

--- Packages/CopyTool/index.py
+++ Packages/CopyTool/index.py
@@ -8,41 +8,36 @@ import sublime_plugin
 MAX_SIZE = 10
 
 
-class CopyToolCommand(sublime_plugin.EventListener):
+class CopyToolCommand(sublime_plugin.TextCommand):
     copyed_queue = []
 
-    def run(self, edit):
-        name = self.view.file_name()
-        suffix = ""
-        index = name.rfind(".")
-
-        if index == -1:
-          suffix = "error_suffix"
+    def run(self, edit, origin_command=None, to_show=None):
+        if origin_command:
+            self.view.run_command(origin_command)
+        if to_show:
+            self.show()
         else:
-          index += 1
-          while index < len(name):
-            suffix += name[index]
-            index += 1
-
-        region = self.view.sel()[0]
-        if region.empty():
-            self.view.show_popup('will using the whole file')
-            return
-        tot_str = self.view.substr(region)
-        
-        if suffix == 'py':
-            result = py_yapf(1, 10, '/home/vimi/aaaavimidatafile/test/python/main.py')
-            print(result)
-        else:
-            self.view.show_popup('Unsupport format file type')
+            copy_value = sublime.get_clipboard()
+            self.put(copy_value)
+            print(self.copyed_queue)
 
     def put(self, value):
         if len(self.copyed_queue) == MAX_SIZE:
             self.copyed_queue = self.copyed_queue[1:]
-        self.append(value)
+        self.copyed_queue.append(value)
 
     def get_all(self):
-        pass
+        return ["{}:   {}".format(idx + 1, v.lstrip()[:80]) for idx, v in enumerate(reversed(self.copyed_queue))]
 
-    def on_clone(self, view):
-        print("copy")
+    def select_idx(self, idx):
+        if idx == -1:
+            return
+        content = self.copyed_queue[len(self.copyed_queue) - idx - 1]
+        sublime.set_clipboard(content)
+        self.view.run_command("paste_and_indent", {"characters": content})
+
+    def show(self):
+        self.view.window().show_quick_panel(
+          self.get_all(),
+          lambda x: self.select_idx(x),
+        )
