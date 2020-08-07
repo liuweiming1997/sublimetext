b97821b17820ad6932bbab91b2c9197273a0bb3f
weimingliu <vimiming@gmail.com>
2020-08-07 23:54:31 +0800

    more filed for network header

--- Packages/DefaultCode/cpp/header/network_header.h
+++ Packages/DefaultCode/cpp/header/network_header.h
@@ -43,3 +43,37 @@ struct udppack {
   struct udphdr upd_1; //8
   unsigned char body[0]; // sizeof(body) = 0，这个长度可以根据udp里面的长度规定
 };
+
+struct tcphdr {
+  unsigned short sport;
+  unsigned short dport;
+
+  unsigned int seqnum;
+  unsigned int acknum;
+  unsigned char hdrlen:4, // 4 * 15
+                resv:4;
+  unsigned char cwr:1,
+                ece:1,
+                urg:1,
+                ack:1,
+                psh:1,
+                rst:1,
+                syn:1,
+                fin:1;
+  unsigned short windows;
+  unsigned short check_sum;
+  unsigned short urgent_port; // 表明从某个位置开始，这个值很重要
+};
+
+
+struct tcb {
+  int s_ip;
+  int d_ip;
+  int s_port;
+  int d_port;
+  char proto;
+
+  int fd;
+  char* rbuf;
+  char* sbuf;
+};
